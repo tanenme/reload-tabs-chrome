3455c57c4025abf433c3b97ceceb480b2ccd3509
tanenme <127464868+tanenme@users.noreply.github.com>
2024-10-31 23:36:19 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,8 +1,12 @@
+function getRandomNumber() {
+  return Math.floor(Math.random() * (220 - 190 + 1)) + 190;
+}
+
 document.getElementById('start').addEventListener('click', () => {
-  const intervalValue = document.getElementById('interval').value;
+  const intervalValue = getRandomNumber()
   chrome.runtime.sendMessage({ action: 'start', interval: intervalValue });
 });
 
 document.getElementById('stop').addEventListener('click', () => {
   chrome.runtime.sendMessage({ action: 'stop' });
-}); 
+});
